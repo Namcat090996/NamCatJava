114910567815047c918f5f0796c1e0fc65b41e88
Namcat090996 <huynhnamcat@gmail.com>
2024-05-13 16:56:33 +0700

    13/05/2024

--- NetBeansProjects/NamCat_BaiTapCauTruc_JV01/src/namcat_baitapcautruc_jv01/NamCat_BaiTapCauTruc_Part1.java
+++ NetBeansProjects/NamCat_BaiTapCauTruc_JV01/src/namcat_baitapcautruc_jv01/NamCat_BaiTapCauTruc_Part1.java
@@ -188,11 +188,265 @@ public class NamCat_BaiTapCauTruc_Part1 {
     {
         System.out.println("Nhập số b khác không");
     }    
-
-    */
     
     //Bài tập 10: In ra trung bình cộng, trung bình nhân của 3 số được nhập vào từ bàn phím:
     
+    //Declare variables:	
+    double a = 0, b = 0, c = 0, trungBinhCong = 0, trungBinhNhan = 0;
+		
+    System.out.print("Nhập số a = ");
+		
+    a = sc.nextDouble();
+		
+    System.out.print("Nhập số b = ");
+		
+    b = sc.nextDouble();
+		
+    System.out.print("Nhập số c = ");
+		
+    c = sc.nextDouble();
+    
+    //Computational processing	
+    trungBinhCong = (a+b+c)/3;
+		
+    trungBinhNhan = Math.cbrt(a+b+c);
+    
+    //Print results	
+    System.out.printf("Trung bình cộng của %.2f, %.2f và %.2f là: %.2f\n", a, b, c, trungBinhCong);
+		
+    System.out.printf("Trung bình nhân của %.2f, %.2f và %.2f là: %.2f\n", a, b, c, trungBinhNhan);
+    
+    //Bài tập 11:Viết chương trình nhập cạnh, bán kính và in ra diện tích, chu vi của các hình: hình vuông, hình chữ nhật, hình tròn.
+    
+    //Declare variables	
+    double canhVuong = 0 , chieuDaiChuNhat = 0, chieuRongChuNhat = 0, banKinh = 0, chuViVuong = 0, chuViChuNhat = 0, chuViTron = 0, dienTichVuong = 0, dienTichChuNhat = 0, dienTichTron = 0;
+    
+    //Take user information entered from the keyboard			
+    System.out.print("Nhập chiều dài cạnh hình vuông = ");
+		
+    canhVuong = sc.nextDouble();
+		
+    System.out.print("Nhập chiều dài hình chữ nhật = ");
+		
+    chieuDaiChuNhat = sc.nextDouble();
+		
+    System.out.print("Nhập chiều rộng hình chữ nhật = ");
+		
+    chieuRongChuNhat = sc.nextDouble();
+    
+    System.out.print("Nhập chiều dài bán kính = ");
+		
+    banKinh = sc.nextDouble();
+    
+    //Computational processing	
+    chuViVuong = 4*canhVuong;
+		
+    chuViChuNhat = 2*(chieuDaiChuNhat+chieuRongChuNhat);
+		
+    chuViTron = 2*Math.PI*banKinh;
+		
+    dienTichVuong = Math.pow(canhVuong, 2);
+				
+    dienTichChuNhat = chieuDaiChuNhat*chieuRongChuNhat;
+		
+    dienTichTron = Math.PI*Math.pow(banKinh, 2);
+    
+    //Print results	
+    System.out.printf("Chu vi hình vuông là = %.2f\n", chuViVuong);
+		
+    System.out.printf("Chu vi hình chữ nhật là = %.2f\n", chuViChuNhat);
+		
+    System.out.printf("Chu vi hình tròn là = %.2f\n", chuViTron);
+		
+    System.out.printf("Diện tích hình vuông là = %.2f\n", dienTichVuong);
+		
+    System.out.printf("Diện tích hình chữ nhật là = %.2f\n", dienTichChuNhat);
+		
+    System.out.printf("Diện tích hình tròn là = %.2f\n", dienTichTron);   
+    
+    //Bài tập 12: Nhập a, b, c là độ dài 3 cạnh của tam giác (chú ý đảm bảo tổng 2 cạnh phải lớn hơn cạnh còn lại). Tính chu vi, diện tích, độ dài 3 đường cao, 3 đường trung tuyến, 3 đường phân giác, bán kính đường tròn nội tiếp, ngoại tiếp lần lượt theo các công thức sau:
+    
+    //Declare variables	
+    double a = 0, b = 0, c = 0, p = 0, chuVi = 0, dienTich = 0, ha = 0, hb = 0, hc = 0, ma = 0, mb = 0, mc = 0, ga = 0, gb = 0, gc = 0, banKinhNoiTiep = 0, banKinhNgoaiTiep = 0;
+    
+    //Take user information entered from the keyboard	
+    System.out.print("Nhập chiều dài cạnh a = ");
+		
+    a = sc.nextDouble();
+		
+    System.out.print("Nhập chiều dài cạnh b = ");
+		
+    b = sc.nextDouble();
+		
+    System.out.print("Nhập chiều dài cạnh c = ");
+		
+    c = sc.nextDouble();
+    
+    //Computational processing and print results	
+    if(a+b>c && a+c>b && b+c>a)
+    {
+	p = (a+b+c)/2;
+			
+	chuVi = 2*p;
+			
+	dienTich = p*(p-a)*(p-b)*(p-c);
+			
+	dienTich = Math.sqrt(dienTich);
+			
+	ha = (2*dienTich)/a; hb = (2*dienTich)/b; hc = (2*dienTich)/c;
+	
+	ma = 2*Math.pow(b, 2) + 2*Math.pow(c, 2) - Math.pow(a, 2); ma = 0.5*Math.sqrt(ma);
+			
+	mb = 2*Math.pow(a, 2) + 2*Math.pow(c, 2) - Math.pow(b, 2); mb = 0.5*Math.sqrt(mb);
+			
+	mc = 2*Math.pow(a, 2) + 2*Math.pow(b, 2) - Math.pow(c, 2); mc = 0.5*Math.sqrt(mc);
+			
+	ga = b*c*p*(p-a); ga = (2/(b+c))*Math.sqrt(ga);
+			
+	gb = a*c*p*(p-b); gb = (2/(a+c))*Math.sqrt(gb);
+			
+	gc = b*a*p*(p-c); gc = (2/(b+a))*Math.sqrt(gc);
+			
+	banKinhNoiTiep = dienTich/p;
+			
+	banKinhNgoaiTiep = (a*b*c)/(4*dienTich);
+			
+	System.out.printf("Chu vi hình tam giác C = %.2f\n", chuVi);			
+	System.out.printf("Diện tích hình tam giác S = %.2f\n", dienTich);
+			
+	System.out.printf("Độ dài đường cao ha = %.2f\n", ha);
+        System.out.printf("Độ dài đường cao hb = %.2f\n", hb);
+        System.out.printf("Độ dài đường cao hc = %.2f\n", hc);
+			
+	System.out.printf("Độ dài đường trung tuyến ma = %.2f\n", ma);
+        System.out.printf("Độ dài đường trung tuyến mb = %.2f\n", mb);
+        System.out.printf("Độ dài đường trung tuyến mc = %.2f\n", mc);
+			
+	System.out.printf("Độ dài đường phân giác ga = %.2f\n", ga);
+        System.out.printf("Độ dài đường phân giác gb = %.2f\n", gb);	
+        System.out.printf("Độ dài đường phân giác gc = %.2f\n", gc);	
+			
+	System.out.printf("Bán kính đường tròn nội tiếp r = %.2f\n", banKinhNoiTiep);
+	System.out.printf("Bán kính đường tròn ngoại tiếp R = %.2f\n", banKinhNgoaiTiep);			
+    }
+    else
+    {
+	System.err.println("Bạn cần phải nhập lại độ dài 3 cạnh của tam giác đảm bảo điều kiện: Tổng 2 cạnh phải lớn hơn cạnh còn lại");
+    }    
+    
+    //Bài tập 13: Tính diện tích và thể tích của hình cầu bán kính R theo công thức:
+		
+    //Declare varibles
+    double banKinh = 0, dienTich = 0, theTich = 0;
+    
+    //Take user information entered from the keyboard	
+    System.out.print("Nhập bán kính R = ");
+    banKinh = sc.nextDouble();
+    
+    //Computational processing and print results	
+    if (banKinh>0)
+    {
+	dienTich = 4*Math.PI*Math.pow(banKinh, 2);
+					
+	theTich = (banKinh*dienTich)/3;
+			
+	System.out.printf("Diện tích của hình cầu là = %.2f\n", dienTich);
+			
+	System.out.printf("Thể tích của hình cầu là = %.2f\n", theTich);
+    }
+    else
+    {
+	System.out.println("Bạn cần nhập lại bán kính R phù hợp với điều kiện = R > 0");
+    } 
+    
+    //Bài tập 14: Nhập vào 4 chữ số. In ra tổng của 4 chữ số này và chữ số hàng chục, hàng đơn vị của tổng (ví dụ 4 chữ số 3, 1, 8, 5 có tổng là 17 và chữ số hàng chục là 1 và hàng đơn vị là 7, cần in ra 17, 1, 7).
+    
+    //Declare variables
+    int a = 0, b = 0, c = 0, d = 0, tongSo = 0, soHangChuc = 0, soHangDonVi = 0; 
+    
+    //Take user information entered from the keyboard
+    System.out.print("Nhập số a = ");	
+    a = sc.nextInt();
+    System.out.print("Nhập số b = ");
+    b = sc.nextInt();
+    System.out.print("Nhập số c = ");
+    c = sc.nextInt();
+    System.out.print("Nhập số d = ");
+    d = sc.nextInt();  
+    
+    //Computational processing
+    tongSo = a + b + c + d;
+    soHangChuc = tongSo/10;
+    soHangDonVi = tongSo%10;
+    
+    //Print results
+    System.out.println("Tổng số là = " + (int)tongSo);
+    System.out.println("Chữ số hàng chục là = " + soHangChuc);
+    System.out.println("Chữ số hàng đơn vị là = " + soHangDonVi); 
+ 
+    //Bài tập 15: Nhập vào một số nguyên (có 4 chữ số). In ra tổng của 4 chữ số này và chữ số đầu, chữ số cuối (ví dụ số 3185 có tổng các chữ số là 17, đầu và cuối là 3 và 5, kết quả in ra là: 17, 3, 5).
+    
+    //Declare variables
+    int soNguyen = 0, tongSo = 0, hangDonVi = 0, hangChuc = 0, hangTram = 0, hangNghin = 0, temp = 0;
+    
+    //Take user information entered from the keyboard
+    System.out.print("Nhập số nguyên = ");	
+    soNguyen = sc.nextInt(); 
+    
+    //Computational processing
+    if(soNguyen <= 9999 && soNguyen > 999)
+    {
+        hangNghin = soNguyen/1000;
+        temp = soNguyen%1000;
+        hangTram = temp/100;
+        temp = temp%100;
+        hangChuc = temp/10;
+        hangDonVi = temp%10;
+        tongSo = hangChuc + hangDonVi + hangTram + hangNghin;
+        System.out.println("Tổng số là = " + tongSo);
+        System.out.println("Chữ số đầu = " + hangNghin);
+        System.out.println("Chữ số cuối = " + hangDonVi);
+    }
+    else
+    {
+        System.err.println("Bạn cần phải nhập lại số nguyên có 04 chữ số");
+    }
             
+    //Bài tập 16: Hãy nhập 2 số a và b. Viết chương trình đổi giá trị của a và b theo 2 cách:
+    //Declare variables
+    
+    //Use auxiliary variables	
+    int a = 30, b = 50;		
+    int t = a; 			
+    a = b;	
+    b = t;	
+    System.out.printf("Giá trị sau khi đổi:\n a = %d\n b = %d\n", a, b);
+    
+    //Not use auxiliary variables  
+    int c = 30, d = 50;		
+    c = c + d;		
+    d = c - d;  
+    c = c - d;
+    System.out.printf("Giá trị sau khi đổi:\n c = %d\n d = %d\n", c, d);  
+    
+    */
+    
+    //Bài tập 17: Một sinh viên gồm có các thông tin: họ tên, tuổi, điểm toán (hệ số 2), điểm tin (hệ số 1). Hãy nhập các thông tin trên cho 2 sinh viên. In ra bảng điểm gồm các chi tiết nêu trên và điểm trung bình của mỗi sinh viên:
+    
+    //Declare variables	
+    String hoTen1 = "Huỳnh Nam Cát", hoTen2 = "Huỳnh Nam Đài";	
+    int tuoi1 = 27, tuoi2 = 31;	
+    double diemToan1 = 6.5, diemToan2 = 7.5, diemTin1 = 7.5, diemTin2 = 8.5;
+
+    //Print results
+    System.out.printf("Tên của sinh viên thứ nhất: %s\n", hoTen1);		
+    System.out.printf("Tuổi của sinh viên thứ nhất: %d\n", tuoi1);		
+    System.out.printf("Điểm toán của sinh viên thứ nhất: %.2f\n", diemToan1);		
+    System.out.printf("Điểm tin của sinh viên thứ nhất: %.2f\n", diemTin1);		
+    System.out.printf("Điểm trung bình của sinh viên thứ nhất: %.2f\n", ((diemToan1*2)+diemTin1)/2);
+    
+    //Bài tập 18: Một nhân viên gồm có các thông tin: họ tên, hệ số lương, phần trăm phụ cấp (theo lưong) và phần trăm phải đóng BHXH. Hãy nhập các thông tin trên cho 2 nhân viên. In ra bảng lương gồm các chi tiết nêu trên và tổng số tiền cuối cùng mỗi nhân viên được nhận.:
+
+    
   }  
 }

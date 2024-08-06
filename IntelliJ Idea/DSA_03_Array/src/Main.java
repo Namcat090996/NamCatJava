08a6bd9f316d480537888cf14d7afe784966adc9
Namcat090996 <167270367+Namcat090996@users.noreply.github.com>
2024-08-06 07:51:42 +0700

    6/8/2024

--- IntelliJ Idea/DSA_03_Array/src/Main.java
+++ IntelliJ Idea/DSA_03_Array/src/Main.java
@@ -4,23 +4,39 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
+        //Declare an object to enter data from the keyboard
         Scanner sc = new Scanner(System.in);
 
         //Bài tập 1:
-
-        //Enter the element quatity of the array
-        System.out.print("Nhập số lượng phần tử của mảng: ");
-        int n = sc.nextInt();
-
-        //Declare an object
         baiTapVeMang bt = new baiTapVeMang();
-        float[] mangSoThuc = bt.taoMangSoThuc(n);
+        System.out.println();
 
-        //Print the array
-        bt.inMang(mangSoThuc);
+        //Bài tập 2:
+        bt.ganX(1.5f);
+        bt.inMang();
+        System.out.println();
 
+        //Bài tập 3:
+        float tong2PhanTuDauTien = bt.tong2PhanTuDauTien();
+        System.out.printf("- Tổng của 2 phần tử đầu tiên là: %.2f\n", tong2PhanTuDauTien);
+
+        //Bài tập 4:
+        float tong = bt.tong();
+        System.out.printf("- Tổng của tất cả các phần tử trong mảng là: %.2f\n", tong);
+
+        //Bài tập 5:
+        float min = bt.giaTriNhoNhat();
+        System.out.printf("- Giá trị phần tử nhỏ nhất trong mảng là: %.2f\n", min);
+
+        //Bài tập 6:
+        bt.taoNgauNhien();
+        System.out.println();
+
+        //Bài tập 7: Array Initializer
+        float[] arr = new float[]{3.5f, 5.5f, 4.52f, 5.6f};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d] = %.2f\t", i, arr[i]);
+        }
     }
 }
--- IntelliJ Idea/DSA_03_Array/src/baiTapVeMang.java
+++ IntelliJ Idea/DSA_03_Array/src/baiTapVeMang.java
@@ -2,14 +2,24 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class baiTapVeMang {
+    //Declare an array
+
+    float[] arr;
+
+    //Declare the elements number of the array
+    int n;
 
     public baiTapVeMang()
     {
+        //Declare an object to enter data from the keyboard
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập số lượng phần tử của mảng: ");
-        
-    }
 
+        //Enter the elements number of the array
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        n = sc.nextInt();
+        arr = taoMangSoThuc(n);
+        inMang();
+    }
 
     /**
      * Function to create an array of float numbers
@@ -18,7 +28,7 @@ public class baiTapVeMang {
      */
     public float[] taoMangSoThuc(int n)
     {
-        //Declare an object
+        //Declare an object to create the random number
         Random rd = new Random();
 
         //Declare an array of float numbers
@@ -33,13 +43,134 @@ public class baiTapVeMang {
         return arr;
     }
 
-    public void inMang(float[] arr)
+    /**
+     * Function to print the array
+     */
+    public void inMang()
     {
         for(int i = 0; i < arr.length; i++)
         {
-            System.out.printf("arr[%d] = %.2f\n", i, arr[i]);
+            System.out.printf("arr[%d] = %.2f\t", i, arr[i]);
         }
     }
+
+    /**
+     * Function to assign x for last element
+     * @param x
+     */
+    public void ganX(float x)
+    {
+        //Increase the elements number of the array
+        n = n+1;
+
+        //Declare a temporary array
+        float[] temp = new float[n];
+
+        //Copy the values from old array to new array
+        for(int i = 0; i < arr.length; i++)
+        {
+            temp[i] = arr[i];
+        }
+
+        //Assign x to last element of temp array
+        temp[n-1] = x;
+
+        //Assign values to array again
+        this.arr = temp;
+    }
+
+    /**
+     * Function to sum of the 1st & 2nd element in the array
+     * @return
+     */
+    public float tong2PhanTuDauTien()
+    {
+        //Declare a variable
+        float tong = 0;
+
+        //Process the computational
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(i < 2)
+            {
+                tong += arr[i];
+            }
+        }
+
+        return tong;
+    }
+
+    /**
+     * Function to sum of the 1st & 2nd element in the array
+     * @return
+     */
+    public float tong2PhanTuDauTienCach2()
+    {
+        //Declare a variable
+        float tong = 0;
+
+        //Process the computational
+        for(int i = 0; i < 2 & i < arr.length; i++)
+        {
+            tong += arr[i];
+        }
+
+        return tong;
+    }
+
+    /**
+     * Function to sum all the elements in the array
+     * @return
+     */
+    public float tong()
+    {
+        //Declare a variable
+        float tong = 0;
+
+        //Process the computational
+        for(int i = 0; i < arr.length; i++)
+        {
+            tong += arr[i];
+        }
+
+        return tong;
+    }
+
+    public float giaTriNhoNhat()
+    {
+        //Declare variable
+        float min = 0;
+
+        //Process the computational
+        min = arr[0];
+
+        for(int i = 1; i < arr.length; i++)
+        {
+            if(arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
+
+    /**
+     * Function to create a random element index in the array
+     */
+    public void taoNgauNhien()
+    {
+        //Declare an object
+        Random rd = new Random();
+
+        //Process the computational
+        int index = rd.nextInt(arr.length); //A random index will have value from 0 to (arr.length - 1)
+
+        //Print the result
+        System.out.printf("- Giá trị phần tử ngẫu nhiên trong mảng arr[%d] = %.2f\t", index, arr[index]);
+    }
+
+    
 
 
 }

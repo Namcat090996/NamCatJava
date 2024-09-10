0d18d0d3123fed4057aea0d286452182ba095282
Namcat090996 <167270367+Namcat090996@users.noreply.github.com>
2024-09-10 20:28:47 +0700

    10/09/2024

--- VS Code/LapTrinhWebCoBan_Thay/js/TinhToan.js
+++ VS Code/LapTrinhWebCoBan_Thay/js/TinhToan.js
@@ -1,19 +1,15 @@
-function TongHaiSo(a, b)
-{
+function TongHaiSo(a, b) {
     return a + b;
 }
 
-function HieuHaiSo(a, b)
-{
+function HieuHaiSo(a, b) {
     return a - b;
 }
 
-function TichHaiSo(a, b)
-{
-    return a * b;
+function ThuongHaiSo(a, b) {
+    return a / b;
 }
 
-function ThuongHaiSo(a, b)
-{
-    return a/b;
+function TichHaiSo(a, b) {
+    return a * b;
 }
